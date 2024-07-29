88403bb0abdf083fd9710ec8c7cf88647839de6d
serejaga <maksimiliansovokockin161@gmail.com>
2024-07-29 21:36:48 +0300

    update

---
+++ gamemode/modules/character/sh_character.lua
@@ -0,0 +1,18 @@
+/* hh character system, made by @surelyreplica */
+gm.char = {}
+
+/* base character fields */
+local base = {
+    -- name field: first, last, nick 
+    name = { first = "John", last = "Doe", nick = "anon" },
+
+    -- age field: biological, chronological
+
+} 
+
+
+
+/* backstory field */
+local backstories = {}
+
+/* traits */
