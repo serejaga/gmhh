2621298b9d38e21300e994935d9ca68734f7a6f8
FaPerson <k57088526@gmail.com>
2024-07-30 21:36:42 +0500

    Update sv_init.lua

--- gamemode/modules/daynight/all/sv_init.lua
+++ gamemode/modules/daynight/all/sv_init.lua
@@ -1,5 +1,8 @@
 util.AddNetworkString("TimeChange")
 time = "day"
+
+
+local skyPaint = ents.FindByClass( "env_skypaint" )[ 1 ]
 function clientchange(ints)
 
 	for k, v in pairs(player.GetAll()) do
@@ -13,7 +16,44 @@ timer.Create("Infinity",3600,0,function()
 	if time == "day" then
 		time = "night"
 		clientchange("nig")
+		skyPaint:SetTopColor( Vector( 0.1, 0.4, 0.9 ) - Vector( 0.2,0.2,0.2 ) )
+		skyPaint:SetBottomColor( Vector( 0.7, 0.9, 0.9 ) - Vector( 0.2,0.2,0.2 ) )
+		skyPaint:SetFadeBias( 1 )
+
+		skyPaint:SetSunNormal( Vector( 0.1, 0.0, 0.01 ) - Vector( 0.2,0.2,0.2 ) )
+		skyPaint:SetSunColor( Vector( 0.2, 0.1, 0.0 ) - Vector( 0.2,0.2,0.2 ) )
+		skyPaint:SetSunSize( 2.0 )
+
+		skyPaint:SetDuskColor( Vector( 1.0, 0.2, 0.0 ) - Vector( 0.2,0.2,0.2 ) )
+		skyPaint:SetDuskScale( 1 )
+		skyPaint:SetDuskIntensity( 1 )
+
+		skyPaint:SetDrawStars( true )
+		skyPaint:SetStarLayers( 1 )
+		skyPaint:SetStarSpeed( 0.01 )
+		skyPaint:SetStarScale( 0.5 )
+		skyPaint:SetStarFade( 1.5 )
+		skyPaint:SetStarTexture( "skybox/starfield" )
+
 	elseif time == "night" then
+				skyPaint:SetTopColor( Vector( 0.1, 0.4, 0.9 ) )
+		skyPaint:SetBottomColor( Vector( 0.7, 0.9, 0.9 ) )
+		skyPaint:SetFadeBias( 1 )
+
+		skyPaint:SetSunNormal( Vector( 0.1, 0.0, 0.01 ) )
+		skyPaint:SetSunColor( Vector( 0.2, 0.1, 0.0 ) )
+		skyPaint:SetSunSize( 2.0 )
+
+		skyPaint:SetDuskColor( Vector( 1.0, 0.2, 0.0 ) )
+		skyPaint:SetDuskScale( 1 )
+		skyPaint:SetDuskIntensity( 1 )
+
+		skyPaint:SetDrawStars( true )
+		skyPaint:SetStarLayers( 1 )
+		skyPaint:SetStarSpeed( 0.01 )
+		skyPaint:SetStarScale( 0.5 )
+		skyPaint:SetStarFade( 1.5 )
+		skyPaint:SetStarTexture( "skybox/starfield" )
 		time = "day"
 		clientchange("day")
 	end
